22d94748ac7d399e40eecb6be9c17aedd5239f5f
gialli24 <a.gialli2409@gmail.com>
2026-02-10 17:00:35 +0100

    Add exercise 2 arrow function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,7 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/* 
 function getInitialsArray(names) {
+    const initialsArray = [];
+
+    for (const name of names) {
+        const initial = name[0];
+        initialsArray.push(initial);
+    }
+
+    return initialsArray;
+} 
+*/
+
+const initialsArray = (names) => {
     const initialsArray = [];
 
     for (const name of names) {
@@ -16,7 +29,8 @@ function getInitialsArray(names) {
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-const initials = getInitialsArray(names);
+/* const initials = getInitialsArray(names); */
+const initials = initialsArray(names)
 console.log(initials);
 
 
