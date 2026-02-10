e85a398bbe1ac7266a05e6a5a19d5f68b2b3d293
gialli24 <a.gialli2409@gmail.com>
2026-02-10 14:39:43 +0100

    Finish exercise 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function getVowelsCount(word) {
+    let count = 0;
+
+    for (const letter of word) {
+        if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
+            count++;
+        }
+    }
+
+    return count;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const vowelsCount = getVowelsCount(word);
+console.log(vowelsCount);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
